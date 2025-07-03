62c7dae7553d92eba80946312cbf89c3a92fcab3
Michael Wycombe <michael.wycombe@gmail.com>
2025-07-02 18:35:47 -0700

    learncpp make et al

---
+++ src/variablesizes.cpp
@@ -0,0 +1,30 @@
+#include <iomanip> // for std::setw (which sets the width of the subsequent output)
+#include <iostream>
+#include <climits> // for CHAR_BIT
+
+int main()
+{
+    std::cout << "A byte is " << CHAR_BIT << " bits\n\n";
+
+    std::cout << std::left; // left justify output
+
+    std::cout << std::setw(16) << "bool:" << sizeof(bool) << " bytes\n";
+    std::cout << std::setw(16) << "char:" << sizeof(char) << " bytes\n";
+    std::cout << std::setw(16) << "short:" << sizeof(short) << " bytes\n";
+    std::cout << std::setw(16) << "int:" << sizeof(int) << " bytes\n";
+    std::cout << std::setw(16) << "long:" << sizeof(long) << " bytes\n";
+    std::cout << std::setw(16) << "long long:" << sizeof(long long) << " bytes\n";
+    std::cout << std::setw(16) << "float:" << sizeof(float) << " bytes\n";
+    std::cout << std::setw(16) << "double:" << sizeof(double) << " bytes\n";
+    std::cout << std::setw(16) << "long double:" << sizeof(long double) << " bytes\n";
+    std::cout << '\n';
+    std::cout << "least 8:  " << sizeof(std::int_least8_t)  * 8 << " bits\n";
+    std::cout << "least 16: " << sizeof(std::int_least16_t) * 8 << " bits\n";
+    std::cout << "least 32: " << sizeof(std::int_least32_t) * 8 << " bits\n";
+    std::cout << '\n';
+    std::cout << "fast 8:  "  << sizeof(std::int_fast8_t)   * 8 << " bits\n";
+    std::cout << "fast 16: "  << sizeof(std::int_fast16_t)  * 8 << " bits\n";
+    std::cout << "fast 32: "  << sizeof(std::int_fast32_t)  * 8 << " bits\n";
+
+    return 0;
+}
